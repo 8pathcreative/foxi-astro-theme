a58877fcc5f77700ab16a766747e545e0b5136c0
Your Name <neilhumphrey@Neils-Laptop.local>
2025-04-16 10:23:51 -0500

    new stuff

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,9 +21,9 @@ export interface Config {
 }
 
 export const configData: Config = {
-	siteTitle: 'Foxi. Tailwind CSS Astro Starter Kit by Oxygenna',
+	siteTitle: 'Neil Humphrey - Design Engineer',
 	siteDescription:
-		'Foxi is a design and development agency that specializes in creating beautiful and functional websites.',
+		'Neil Humphrey is a design engineer with a passion for creating beautiful and functional websites.',
 	ogImage: '/og.jpg',
 	logo: {
 		src: '/logo.svg',
